892aa9384d335983591e6e06b3b720f8bd51198e
Akshay Deshraj <akshay30nov@gmail.com>
2016-08-07 14:51:23 +0530

    basic stock widgt

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockAppWidgetProvider.java
@@ -0,0 +1,62 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * @author akshay
+ * @since 2/8/16
+ */
+
+public class StockAppWidgetProvider extends AppWidgetProvider {
+
+    public static RemoteViews getRemoteView(Context context, AppWidgetManager appWidgetManager) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stockhawk_appwidget);
+        views.setTextViewText(R.id.widget_text, context.getString(R.string.appwidget_text));
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, StockAppRemoteViewsService.class));
+
+        //Intent to start MyStocksActivity
+
+        Intent stocksActivityIntent = new Intent(context, MyStocksActivity.class);
+        PendingIntent stocksActivityPendingIntent = PendingIntent.getActivity(context, 0,
+                stocksActivityIntent, 0);
+
+        views.setOnClickPendingIntent(R.id.widget_text, stocksActivityPendingIntent);
+
+        return views;
+    }
+
+    public static void updateWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        appWidgetManager.updateAppWidget(appWidgetId, getRemoteView(context, appWidgetManager));
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+}
